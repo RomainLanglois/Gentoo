836204d8313bf32427833354a3a1377aba7f86f9
RomainLanglois <rls_it@protonmail.com>
2022-06-24 12:08:02 +0200

    Major update

--- Bash_Scripts/Install_softwares.sh
+++ Bash_Scripts/Install_softwares.sh
@@ -1,30 +1,35 @@
-# This list is based on my current /var/lib/portage/world file
 # Bash one liner to retrieve and format all packages inside the world file
 # while read -r line; do echo "\"$line\""; done < /var/lib/portage/world
 
-softwares_array=(
-"app-admin/sudo"
+## Add support for the TPM
+TPM_softwares_array=(
 "app-crypt/clevis"
 "app-crypt/tpm2-tools"
 "app-crypt/tpm2-tss"
+)
+
+softwares_array=(
 "app-eselect/eselect-repository"
 "app-misc/neofetch"
 "app-portage/gentoolkit"
-"app-xemacs/emerge"
 "dev-vcs/git"
-"net-misc/dhcpcd"
-"net-misc/netifrc"
 "sys-apps/usbutils"
-"sys-boot/grub:2"
-"sys-fs/cryptsetup"
-"sys-fs/lvm2"
-"sys-kernel/dracut"
-"sys-kernel/gentoo-sources"
-"sys-kernel/linux-firmware"
 "sys-process/htop"
+"app-arch/lz4"
 )
 
 emaint -a sync
 for software in ${softwares_array[@]}; do
   emerge -q $software
 done
+
+echo "Do you wan to install the TPM packages ? (Y/N)"
+read user_choice
+if [[ $user_choice = "Y" ]]
+then
+	for TPM_software in ${TPM_softwares_array[@]}; do
+	  emerge -q $TPM_software
+	done
+else
+	echo "Exiting..."
+fi
