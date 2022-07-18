862662bf8aecab5145e97d4eecd7ab2f4c8695db
RomainLanglois <rls_it@protonmail.com>
2022-07-18 21:02:58 +0200

    Renaming file

--- Bash_Scripts/SELinux/create_policy_SELinux.sh
+++ Bash_Scripts/SELinux/create_policy_SELinux.sh
@@ -7,7 +7,7 @@ policy_file=$(basename $1 .te)
 
 usage ()
 {
-  /bin/echo -e "${RED} ERROR! Usage:"
+  /bin/echo -e "${RED}ERROR! Usage:"
   /bin/echo -e "  $0 <name_of_SELinux_policy>"
   /bin/echo -e "  Example: $0 allow_xorg_user-u.te"
   exit 1
@@ -15,7 +15,7 @@ usage ()
 
 compile_and_install_SELinux_policy ()
 {
-	/bin/echo -e "${GREEN} [*] Compiling and installing new policy ${NC}"
+	/bin/echo -e "${GREEN}[*] Compiling and installing new policy ${NC}"
 	/usr/bin/make -f /usr/share/selinux/strict/include/Makefile $1.pp && /usr/sbin/semodule -i $1.pp && /bin/echo -e "${GREEN} [*] Done ! ${NC}"
 }
 
@@ -26,8 +26,8 @@ fi
 
 if [[ $(/usr/sbin/semodule -l | /bin/grep $policy_file) ]];
 then
-  /bin/echo -e "${RED} [*] $policy_file FOUND !!!, removing old one ! ${NC}"
-  /usr/sbin/semodule -r $policy_file && /bin/echo -e "${GREEN} [*] Done ! ${NC}"
+  /bin/echo -e "${RED} [*]$policy_file FOUND !!!, removing old one ! ${NC}"
+  /usr/sbin/semodule -r $policy_file && /bin/echo -e "${GREEN}[*] Done ! ${NC}"
 fi
 
 /bin/echo -e "Do you want to create a policy based on the audit.log file(1) OR use your own custom policy file(2) ?"
@@ -40,7 +40,7 @@ elif [[ $user_choice == "2" ]];
 then
 	compile_and_install_SELinux_policy $policy_file
 else
-	/bin/echo -e "${RED} Please select 1 OR 2 ${NC}"
+	/bin/echo -e "${RED}Please select 1 OR 2 ${NC}"
 	exit 1
 fi
 
