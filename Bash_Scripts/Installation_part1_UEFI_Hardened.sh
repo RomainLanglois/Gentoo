8bcbc4b2376002c7873fa93cb0c6c025c177edf6
RomainLanglois <rls_it@protonmail.com>
2022-08-02 20:54:15 +0200

    Add support for EFI stub kernel and add && to validate previous commands

--- Bash_Scripts/Installation_part1_UEFI_Hardened.sh
+++ Bash_Scripts/Installation_part1_UEFI_Hardened.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No colors
+
 script_github_path=https://raw.githubusercontent.com/RomainLanglois/Gentoo/main/Bash_Scripts/
 scripts_array=(
 	"Installation_part2_UEFI_Hardened.sh"
 	"Install_GUI_XORG.sh"
 	"Install_CLI_softwares.sh"
 	"Install_Configure_Bwrap-Firewall-USB.sh"
+	"Install_and_configure_secureBoot.sh"
 )
 make_file=/mnt/gentoo/etc/portage/make.conf
 
@@ -15,38 +20,35 @@ make_file=/mnt/gentoo/etc/portage/make.conf
 /bin/echo "[?] Please enter the disk to partition (ex: sda):"
 read disk
 /sbin/wipefs -a /dev/$disk
-/usr/sbin/parted -a optimal /dev/$disk -s 'mklabel gpt'
-/usr/sbin/parted -a optimal /dev/$disk -s 'unit mib'
-/usr/sbin/parted -a optimal /dev/$disk -s 'mkpart primary 1 3'
-/usr/sbin/parted -a optimal /dev/$disk -s 'name 1 grub'
-/usr/sbin/parted -a optimal /dev/$disk -s 'set 1 bios_grub on'
-/usr/sbin/parted -a optimal /dev/$disk -s 'mkpart primary fat32 3 515 '
-/usr/sbin/parted -a optimal /dev/$disk -s 'name 2 boot'
-/usr/sbin/parted -a optimal /dev/$disk -s 'set 2 boot on'
-/usr/sbin/parted -a optimal /dev/$disk -s 'mkpart primary 515 -1'
-/usr/sbin/parted -a optimal /dev/$disk -s 'name 3 lvm'
-/usr/sbin/parted -a optimal /dev/$disk -s 'set 3 lvm on'
+/usr/sbin/parted -a optimal /dev/$disk -s 'mklabel gpt' && \
+/usr/sbin/parted -a optimal /dev/$disk -s 'unit mib' && \
+/usr/sbin/parted -a optimal /dev/$disk -s 'mkpart primary fat32 1 515 ' && \
+/usr/sbin/parted -a optimal /dev/$disk -s 'name 1 boot' && \
+/usr/sbin/parted -a optimal /dev/$disk -s 'set 1 boot on' && \
+/usr/sbin/parted -a optimal /dev/$disk -s 'mkpart primary 515 -1' && \
+/usr/sbin/parted -a optimal /dev/$disk -s 'name 2 lvm' && \
+/usr/sbin/parted -a optimal /dev/$disk -s 'set 2 lvm on' && \
 /usr/sbin/parted -a optimal /dev/$disk -s 'print'
 
 /bin/echo "###########################################"
 /bin/echo "[*] Formating encrypting and mounting the luks partition"
 /bin/lsblk
 /sbin/modprobe dm-crypt
-/bin/echo "[?] Please enter the partion to encrypt with luks (ex: sda3):"
+/bin/echo "[?] Please enter the partion to encrypt with luks (ex: sda2):"
 read luks_partition
-/sbin/cryptsetup luksFormat /dev/$luks_partition
-/sbin/cryptsetup luksOpen /dev/$luks_partition lvm
-/sbin/lvm pvcreate /dev/mapper/lvm
-/sbin/vgcreate vg0 /dev/mapper/lvm
-/sbin/lvcreate -L 40G -n root vg0
-/sbin/lvcreate -l 100%FREE -n home vg0
-/usr/sbin/mkfs.fat -F 32 /dev/$disk\2
-/sbin/mkfs.ext4 /dev/mapper/vg0-root
-/sbin/mkfs.ext4 /dev/mapper/vg0-home
-/bin/mount /dev/mapper/vg0-root /mnt/gentoo
-/bin/mkdir /mnt/gentoo/home
-/bin/mount /dev/mapper/vg0-home /mnt/gentoo/home
-/bin/echo "[*] Done !"
+/sbin/cryptsetup luksFormat /dev/$luks_partition && \
+/sbin/cryptsetup luksOpen /dev/$luks_partition lvm && \
+/sbin/lvm pvcreate /dev/mapper/lvm && \
+/sbin/vgcreate vg0 /dev/mapper/lvm && \
+/sbin/lvcreate -L 40G -n root vg0 && \ 
+/sbin/lvcreate -l 100%FREE -n home vg0 && \
+/usr/sbin/mkfs.fat -F 32 /dev/$disk\1 && \
+/sbin/mkfs.ext4 /dev/mapper/vg0-root && \
+/sbin/mkfs.ext4 /dev/mapper/vg0-home && \ 
+/bin/mount /dev/mapper/vg0-root /mnt/gentoo && \
+/bin/mkdir /mnt/gentoo/home && \
+/bin/mount /dev/mapper/vg0-home /mnt/gentoo/home && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "############################################"
 
 /bin/echo "############################################"
@@ -54,17 +56,17 @@ read luks_partition
 /bin/date
 /bin/echo "[?] Please enter the current time (Example: 20:59:00):"
 read date
-/bin/date -s "$date"
-/bin/echo "[*] Done !"
+/bin/date -s "$date" && \ 
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "############################################"
 
 /bin/echo "############################################"
 /bin/echo "[*] Downloading and decompressing Stage3 TarBall"
-cd /mnt/gentoo
-/usr/bin/links https://www.gentoo.org/downloads/mirrors/
-/bin/tar xpvf stage3-amd64-hardened-*.tar.xz --xattrs-include='*.*' --numeric-owner
-/bin/rm -f stage3-amd64-hardened-*.tar.xz
-/bin/echo "[*] Done !"
+cd /mnt/gentoo && \
+/usr/bin/links https://www.gentoo.org/downloads/mirrors/ && \
+/bin/tar xpvf stage3-amd64-hardened-*.tar.xz --xattrs-include='*.*' --numeric-owner && \
+/bin/rm -f stage3-amd64-hardened-*.tar.xz && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "############################################"
 
 /bin/echo "############################################"
@@ -74,20 +76,22 @@ read user_choice
 if [[ $user_choice = "Y" ]]
 then
 	/bin/echo "[*] Going for a custom one !"
-	/bin/rm -f /mnt/gentoo/etc/portage/make.conf
-	cd /mnt/gentoo/etc/portage/
-	/usr/bin/wget https://raw.githubusercontent.com/RomainLanglois/Gentoo/main/Configuration_files/make.conf
-	/bin/sed -i "s#MAKEOPTS=\"\"#MAKEOPTS=\"-j$(nproc)\"#g" $make_file
-	cd /mnt/gentoo
+	/bin/rm -f /mnt/gentoo/etc/portage/make.conf && \
+	cd /mnt/gentoo/etc/portage/ && \
+	/usr/bin/wget https://raw.githubusercontent.com/RomainLanglois/Gentoo/main/Configuration_files/make.conf && \
+	/bin/sed -i "s#MAKEOPTS=\"\"#MAKEOPTS=\"-j$(nproc)\"#g" $make_file && \
+	cd /mnt/gentoo && \
+	/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
+	/bin/echo "############################################"
 else
 	/bin/echo "Going for a Generic one !"
-	/bin/echo 'COMMON_FLAGS="-march=native -O2 -pipe"' >> $make_file
-	/bin/echo 'GRUB_PLATFORMS="efi-64"' >> $make_file
-	/bin/echo "MAKEOPTS=\"-j$(nproc)\"" >> $make_file
-	/bin/echo 'USE="-systemd -ipv6"' >> $make_file
+	/bin/echo 'COMMON_FLAGS="-march=native -O2 -pipe"' >> $make_file && \
+	/bin/echo 'GRUB_PLATFORMS="efi-64"' >> $make_file && \
+	/bin/echo "MAKEOPTS=\"-j$(nproc)\"" >> $make_file && \
+	/bin/echo 'USE="-systemd -ipv6"' >> $make_file && \
+	/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
+	/bin/echo "############################################"
 fi
-/bin/echo "[*] Done !"
-/bin/echo "############################################"
 
 /bin/echo "############################################"
 /bin/echo "[*] Configuring mirrors"
@@ -95,33 +99,33 @@ if [[ $user_choice = "N" ]]
 then
 	/usr/bin/mirrorselect -i -o >> $make_file
 fi
-/bin/mkdir --parents etc/portage/repos.conf
-/bin/cp usr/share/portage/config/repos.conf etc/portage/repos.conf/gentoo.conf
-/bin/cp --dereference /etc/resolv.conf etc/
-/bin/echo "[*] Done !"
+/bin/mkdir --parents etc/portage/repos.conf && \
+/bin/cp usr/share/portage/config/repos.conf etc/portage/repos.conf/gentoo.conf && \
+/bin/cp --dereference /etc/resolv.conf etc/ && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "############################################"
 
 /bin/echo "############################################"
 /bin/echo "[*] Moving inside chroot !"
-/bin/mount --types proc /proc /mnt/gentoo/proc
-/bin/mount --rbind /sys /mnt/gentoo/sys
-/bin/mount --make-rslave /mnt/gentoo/sys
-/bin/mount --rbind /dev /mnt/gentoo/dev
-/bin/mount --make-rslave /mnt/gentoo/dev
-/bin/mount --bind /run /mnt/gentoo/run
+/bin/mount --types proc /proc /mnt/gentoo/proc && \
+/bin/mount --rbind /sys /mnt/gentoo/sys && \
+/bin/mount --make-rslave /mnt/gentoo/sys && \
+/bin/mount --rbind /dev /mnt/gentoo/dev && \
+/bin/mount --make-rslave /mnt/gentoo/dev && \
+/bin/mount --bind /run /mnt/gentoo/run && \
 /bin/mount --make-slave /mnt/gentoo/run
 /bin/echo "[*] Downloading other installation scripts"
-/bin/mkdir /mnt/gentoo/scripts && \
+/bin/mkdir /mnt/gentoo/scripts
 for script in ${scripts_array[@]}; do
 	  /usr/bin/wget --quiet $script_github_path$script -O /mnt/gentoo/scripts/$script && \
 	  /bin/chmod +x /mnt/gentoo/scripts/$script && \
 	  /bin/echo -e "${GREEN}[*] Script: $script correctly downloaded and configured ! ${NC}"	
 done
-cd /mnt/gentoo/scripts && /usr/bin/git clone https://github.com/RomainLanglois/I3-configuration.git
+cd /mnt/gentoo/scripts && \
 # chroot /mnt/gentoo /bin/bash 
 /usr/bin/chroot /mnt/gentoo/ ./scripts/Installation_part2_UEFI_Hardened.sh
-source /etc/profile
-cd
-/bin/umount /mnt/gentoo/home
-/bin/umount -R /mnt/gentoo
+source /etc/profile && \
+cd && \
+/bin/umount /mnt/gentoo/home && \
+/bin/umount -R /mnt/gentoo && \
 /sbin/reboot
--- Bash_Scripts/Installation_part2_UEFI_Hardened.sh
+++ Bash_Scripts/Installation_part2_UEFI_Hardened.sh
@@ -1,50 +1,52 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No colors
+
 /bin/echo "########################################"
 /bin/echo "[*] Preparing the environment and emerging @world"
-source /etc/profile
-export PS1="(chroot) ${PS1}"
-/bin/lsblk
-/bin/echo "[?] Please enter disk partition where to mount boot (ex: sda2):"
-read boot_partition
-/bin/mount /dev/$boot_partition /boot
-/usr/bin/emerge-webrsync
+source /etc/profile && \
+export PS1="(chroot) ${PS1}" && \
+/bin/mount $(/sbin/blkid | /bin/grep boot | /usr/bin/cut -d ":" -f1) /boot && \
+/usr/bin/emerge-webrsync && \
 /usr/bin/eselect profile list
 /bin/echo "[?] Please select your profile:"
 read profile
-/usr/bin/eselect profile set $profile
-/usr/bin/emerge --verbose --update --deep --newuse @world
-/bin/echo "[*] Done !"
+/usr/bin/eselect profile set $profile && \
+/usr/bin/emerge --verbose --update --deep --newuse @world && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
 /bin/echo "[*] Configuring timezone and locale"
-/bin/echo "Europe/Paris" > /etc/timezone
-/bin/echo "en_US ISO-8859-1" >> /etc/locale.gen
-/bin/echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-/bin/echo "fr_FR ISO-8859-1" >> /etc/locale.gen
-/bin/echo "fr_FR.UTF-8 UTF-8" >> /etc/locale.gen
+/bin/echo "Europe/Paris" > /etc/timezone && \
+/bin/echo "en_US ISO-8859-1" >> /etc/locale.gen && \
+/bin/echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && \
+/bin/echo "fr_FR ISO-8859-1" >> /etc/locale.gen && \
+/bin/echo "fr_FR.UTF-8 UTF-8" >> /etc/locale.gen && \
 /usr/sbin/locale-gen
 /usr/bin/eselect locale list
 /bin/echo "[?] Please select your locale:"
 read locale
-/usr/bin/eselect locale set $locale
-env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
-/bin/echo "[*] Done !"
+/usr/bin/eselect locale set $locale && \
+env-update && source /etc/profile && export PS1="(chroot) ${PS1}" && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
 /bin/echo "[*] Configuring the keymap"
-/bin/sed -i 's/keymap=\"us\"/keymap=\"fr\"/g' /etc/conf.d/keymaps
-/bin/echo "[*] Done !"
+/bin/sed -i 's/keymap=\"us\"/keymap=\"fr\"/g' /etc/conf.d/keymaps && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
 /bin/echo "[*] Installing / Configuring linux firmware, kernel sources and initramfs"
-/bin/echo "sys-kernel/linux-firmware linux-fw-redistributable no-source-code" >> /etc/portage/package.license
-/usr/bin/emerge -q sys-kernel/linux-firmware
-/usr/bin/emerge -q sys-kernel/gentoo-sources
-/usr/bin/emerge -q sys-fs/cryptsetup sys-fs/lvm2
-/usr/bin/emerge -q app-arch/lz4
+/bin/echo "sys-kernel/linux-firmware linux-fw-redistributable no-source-code" >> /etc/portage/package.license && \
+/usr/bin/emerge -q sys-kernel/linux-firmware && \
+/usr/bin/emerge -q sys-kernel/gentoo-sources && \
+/usr/bin/emerge -q sys-fs/cryptsetup sys-fs/lvm2 && \
+/usr/bin/emerge -q app-arch/lz4 && \
 /usr/bin/eselect kernel list
 /bin/echo "[?] Please select the kernel:"
 read kernel
@@ -54,84 +56,106 @@ cd /usr/src/linux
 read user_choice
 if [[ $user_choice = "Y" ]]
 then
-	/usr/bin/emerge -q sys-kernel/genkernel
-	/usr/bin/genkernel --luks --lvm --no-zfs all
-	/usr/bin/genkernel --luks --lvm --compress-initramfs-type=lz4 initramfs
+	/bin/echo "[*] Going for a generic kernel"
+	/usr/bin/emerge -q sys-kernel/genkernel && \
+	/usr/bin/genkernel --luks --lvm --no-zfs all && \
+	/usr/bin/genkernel --luks --lvm --compress-initramfs-type=lz4 initramfs && \
+	/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
+	/bin/echo "########################################"
 else
-	cd /usr/src/linux
-	make menuconfig
-	#/usr/bin/wget https://raw.githubusercontent.com/RomainLanglois/Gentoo/main/Configuration_files/config_kernel_5-15-41.config
-	#/bin/mv config_kernel_*.config .config
-	make -j$(nproc) && make modules_install && make install
-	/usr/bin/emerge -q sys-kernel/genkernel
-	/usr/bin/genkernel --luks --lvm --kernel-config=/usr/src/linux/.config --compress-initramfs-type=lz4 initramfs
-
-	# Dra/usr/bin/cut à finaliser lors de l'installation et configuration du TPM
-	#/usr/bin/emerge -q sys-kernel/dracut
-	#/bin/echo "add_dra/usr/bin/cutmodules+=" lvm crypt "" >> /etc/dracut.conf
-	#/bin/echo "use_fstab="yes"" >> /etc/dracut.conf
-	## Trouver un moyen de compresser l'initramfs
-	## -> Pas sur si fonctionnel
-	#/bin/echo "compress="lz4"" >> /etc/dracut.conf
-	## ls /lib/modules/5.15.41-gentoo
-	#dracut --kver $(uname -a | /usr/bin/cut -f3 -d " ")
+	## Ne pas utiliser cette partie, NON fonctionnel !!!
+	## TODO 
+	/bin/echo "[*] Downloading a custom kernel from github"
+	/usr/bin/emerge -q sys-kernel/genkernel && \
+	cd /usr/src/linux && \
+	# l'initramfs est déjà harcodé dans la config du kernel !!!
+	wget initramfs_data.cpio && \
+	/usr/bin/wget https://raw.githubusercontent.com/RomainLanglois/Gentoo/main/Configuration_files/config_kernel_XXXX.config && \
+	/bin/mv config_kernel_*.config .config && \
+	/usr/bin/genkernel --luks --lvm --kernel-config=/usr/src/linux/.config --no-compress-initramfs initramfs && \
+	mv vmlinuz-5.15.52-gentoo-x86_64 initramfs_data.cpio && \
+	make -j$(nproc) && make modules_install && make install && \
+	/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
+	/bin/echo "########################################"
+	## TODO
 fi
-/bin/echo "[*] Done !"
-/bin/echo "########################################"
 
 /bin/echo "########################################" 
 /bin/echo "[*] DHCP configuration:"
 network_interface=$(/bin/ip a | /bin/grep -i up | /bin/grep -v lo | /usr/bin/cut -d ":" -f2 | sed 's/ //g')
-/usr/bin/emerge --noreplace --quiet net-misc/netifrc
-/bin/echo "config_$network_interface=dhcp" >> /etc/conf.d/net
-/usr/bin/emerge -q net-misc/dhcpcd
-cd /etc/init.d
-/bin/ln -s net.lo "net.$network_interface"
-/sbin/rc-update add "net.$network_interface" default
-/bin/echo "[*] Done !"
+/usr/bin/emerge --noreplace --quiet net-misc/netifrc && \
+/bin/echo "config_$network_interface=dhcp" >> /etc/conf.d/net && \
+/usr/bin/emerge -q net-misc/dhcpcd && \
+cd /etc/init.d && \
+/bin/ln -s net.lo "net.$network_interface" && \
+/sbin/rc-update add "net.$network_interface" default && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
 /bin/echo "[*] Configuring /etc/fstab file"
-/bin/echo "UUID=$(/sbin/blkid | /bin/grep boot | /usr/bin/cut -d "\"" -f2)            /boot           vfat            noauto,noatime  1 2" >> /etc/fstab
-/bin/echo "UUID=$(/sbin/blkid | /bin/grep vg0-root | /usr/bin/cut -d "\"" -f2)       /               ext4            defaults        0 1" >> /etc/fstab
-/bin/echo "UUID=$(/sbin/blkid | /bin/grep vg0-home | /usr/bin/cut -d "\"" -f2)       /home           ext4            defaults        0 1" >> /etc/fstab
-/bin/echo "[*] Done !"
+/bin/echo "UUID=$(/sbin/blkid | /bin/grep boot | /usr/bin/cut -d "\"" -f2)            /boot           vfat            noauto,noatime  1 2" >> /etc/fstab && \
+/bin/echo "UUID=$(/sbin/blkid | /bin/grep vg0-root | /usr/bin/cut -d "\"" -f2)       /               ext4            defaults        0 1" >> /etc/fstab && \
+/bin/echo "UUID=$(/sbin/blkid | /bin/grep vg0-home | /usr/bin/cut -d "\"" -f2)       /home           ext4            defaults        0 1" >> /etc/fstab && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
-/bin/echo "[*] Installing and configuring the bootloader (grub)"
-/bin/rm -rf /etc/portage/package.use/
-/bin/echo "sys-boot/grub:2 device-mapper" >> /etc/portage/package.use
-/usr/bin/emerge -q sys-boot/grub:2
-luks_container=$(/sbin/blkid | /bin/grep -i luks | /usr/bin/cut -d " " -f 2)
-/bin/sed -i "s/\#GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"dolvm crypt_root=$luks_container keymap=fr\"/g" /etc/default/grub
-/usr/sbin/grub-install --target=x86_64-efi --efi-directory=/boot
-/usr/sbin/grub-mkconfig -o /boot/grub/grub.cfg
-/sbin/rc-update add lvm boot
-/sbin/rc-update add dmcrypt boot
-/bin/echo "[*] Done !"
-/bin/echo "########################################"
+/bin/echo "[*] Installing and configuring the bootloader"
+/bin/echo "[?] Do you wand to use a EFI stub kernel as bootloader (Y/N) ?"
+/bin/echo -e "${RED}[*] CAUTION ! Your kernel need to support EFI stub in order to boot without a bootloader like grub ${NC}"
+/bin/echo -e "${RED}[*] CAUTION ! If your kernel needs an initramfs make sure it is harcoded inside the kernel ! Else it will not boot ! ${NC}"
+read user_choice
+if [[ $user_choice == "Y" ]]
+then
+	/bin/echo "########################################"
+	/bin/echo "[*] Installing and configuring EFI stub"
+	/usr/bin/emerge -q sys-boot/efibootmgr && \
+	/bin/mkdir -p /boot/efi/gentoo /boot/efi/gentoo/rescue && \
+	/bin/cp /boot/vmlinuz-$(uname -r) /boot/efi/gentoo/bzImage-$(uname -r).efi && \
+	/bin/echo "[*] Generating a EFI stub rescue" && \
+	/bin/cp /boot/vmlinuz-$(uname -r) /boot/efi/gentoo/rescue/bzImage-$(uname -r).efi && \
+	/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
+	/bin/echo "[*] Creating EFI stub entries" && \
+	/usr/sbin/efibootmgr --create --disk /dev/sda --part 1 --label "Gentoo" --loader "\efi\gentoo\bzImage-$(uname -r).efi" && \
+	/usr/sbin/efibootmgr --create --disk /dev/sda --part 1 --label "Gentoo_rescue" --loader "\efi\gentoo\rescue\bzImage-$(uname -r).efi" && \
+	/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
+	/bin/echo "########################################"
+else
+	/bin/echo "########################################"
+	/bin/echo "[*] Installing and configuring Grub"
+	/bin/rm -rf /etc/portage/package.use/ && \
+	/bin/echo "sys-boot/grub:2 device-mapper" >> /etc/portage/package.use && \
+	/usr/bin/emerge -q sys-boot/grub:2 && \
+	luks_container=$(/sbin/blkid | /bin/grep -i luks | /usr/bin/cut -d " " -f 2) && \
+	/bin/sed -i "s/\#GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"dolvm crypt_root=$luks_container keymap=fr\"/g" /etc/default/grub && \
+	/usr/sbin/grub-install --target=x86_64-efi --efi-directory=/boot && \
+	/usr/sbin/grub-mkconfig -o /boot/grub/grub.cfg && \
+	/sbin/rc-update add lvm boot && \
+	/sbin/rc-update add dmcrypt boot && \
+	/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
+	/bin/echo "########################################"
+fi
 
 /bin/echo "########################################"
 /bin/echo "[*] Changing box name and adding/configuring a new user"
 /bin/echo "[?] Please enter a name for the box:"
 read box_name
-/bin/sed -i "s/hostname=\"localhost\"/hostname=\'$box_name\'/g" /etc/conf.d/hostname
+/bin/sed -i "s/hostname=\"localhost\"/hostname=\'$box_name\'/g" /etc/conf.d/hostname && \
 /usr/bin/emerge -q app-admin/sudo
 /bin/echo "Please enter the username which will be used for this system:"
 read username
-/usr/sbin/useradd -m -G wheel -s /bin/bash $username
-/usr/bin/passwd
-/usr/bin/passwd $username
-/bin/echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
-/bin/echo "[*] Done !"
+/usr/sbin/useradd -m -G wheel -s /bin/bash $username && \
+/usr/bin/passwd && \
+/usr/bin/passwd $username && \
+/bin/echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
 /bin/echo "[*] Removing annoying beep sound"
-/bin/sed -i "s#\# set bell-style none#set bell-style none#g" /etc/inputrc
-/bin/echo "[*] Done !"
+/bin/sed -i "s#\# set bell-style none#set bell-style none#g" /etc/inputrc && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
@@ -139,23 +163,23 @@ read username
 /bin/date
 /bin/echo "[?] Please enter the current time (Example : 20:59:00):"
 read time
-/bin/date -s "$time"
-/sbin/hwclock --systohc
-/bin/echo "[*] Done !"
+/bin/date -s "$time" && \
+/sbin/hwclock --systohc && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
 /bin/echo "[*] Installing auditd"
-/usr/bin/emerge -q sys-process/audit
-/sbin/rc-update add auditd default
-/sbin/rc-service auditd start
-/bin/echo "[*] Done !"
+/usr/bin/emerge -q sys-process/audit && \
+/sbin/rc-update add auditd default && \
+/sbin/rc-service auditd start && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
 
 /bin/echo "########################################"
 /bin/echo "[*] Umounting the environment"
-cd
-/bin/umount /dev/$boot_partition
-/bin/lsblk
-/bin/echo "[*] Done !"
+cd && \
+/bin/umount $(/sbin/blkid | /bin/grep boot | /usr/bin/cut -d ":" -f1) && \
+/bin/lsblk && \
+/bin/echo -e "${GREEN}[*] Done ! ${NC}" && \
 /bin/echo "########################################"
