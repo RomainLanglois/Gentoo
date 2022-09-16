23d8c74357c97e2bd655e4d7750714d51dabc96f
RomainLanglois <rls_it@protonmail.com>
2022-09-16 13:59:59 +0200

    Make sandbox compatible with Gentoo

--- Bash_Scripts/bwrap_profiles/sandbox_evince.sh
+++ Bash_Scripts/bwrap_profiles/sandbox_evince.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
 exec bwrap \
-   --ro-bind /usr/bin/evince /usr/bin/evince \
-   --ro-bind /lib/x86_64-linux-gnu /lib/x86_64-linux-gnu \
-   --ro-bind /usr /usr \
-   --ro-bind /var /var \
-   --symlink /lib/x86_64-linux-gnu /lib64 \
-   --ro-bind $HOME/Documents $HOME/Documents \
-   --ro-bind $HOME/.Xauthority $HOME/.Xauthority \
-   --ro-bind /etc/fonts /etc/fonts \
-   --ro-bind /tmp/.X11-unix/X0 /tmp/.X11-unix/X0 \
-   --unshare-all \
-   --new-session \
+     --ro-bind /usr/bin/evince /usr/bin/evince \
+     --ro-bind /usr/lib64 /usr/lib64 \
+     --ro-bind /lib64 /lib64 \
+     --ro-bind /usr/lib /usr/lib \
+     --ro-bind /usr/share /usr/share \
+     --ro-bind /etc /etc \
+     --ro-bind ~/Documents/pdf ~/pdf \
+     --proc /proc \
+     --dev /dev \
+     --ro-bind /run/user/"$(id -u)"/wayland-1 /run/user/"$(id -u)"/wayland-1 \
+     --ro-bind ~/.cache/fontconfig ~/.cache/fontconfig \
+     --ro-bind "${@: -1}" ~/"$(basename "${@: -1}")" \
+     --chdir ~/ \
+     --unshare-all \
+     --new-session \
 /usr/bin/evince "$@"
