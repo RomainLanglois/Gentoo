bcb548378746bb98879c33a0810d7b16ead65a50
RomainLanglois <rls_it@protonmail.com>
2022-09-23 15:36:45 +0200

    Remove support for TPM and add lsof software

--- Bash_Scripts/02-Install_CLI_softwares.sh
+++ Bash_Scripts/02-Install_CLI_softwares.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-## Add support for the TPM
-TPM_softwares_array=(
-"app-crypt/clevis"
-"app-crypt/tpm2-tools"
-"app-crypt/tpm2-tss"
-)
-
 softwares_array=(
 "app-eselect/eselect-repository"
 "app-misc/neofetch"
@@ -20,7 +13,8 @@ softwares_array=(
 "sys-fs/ntfs3g"
 "dev-util/ltrace"
 "dev-util/strace" 
-"app-arch/p7zip" 
+"app-arch/p7zip"
+"sys-process/lsof"
 )
 
 if [[ "$EUID" -ne 0 ]];
@@ -33,14 +27,3 @@ fi
 for software in ${softwares_array[@]}; do
   /usr/bin/emerge -q $software
 done
-
-/bin/echo "[*] Do you wan to install the TPM packages ? (Y/N)"
-read user_choice
-if [[ $user_choice = "Y" ]];
-then
-	for TPM_software in ${TPM_softwares_array[@]}; do
-	  /usr/bin/emerge -q $TPM_software
-	done
-else
-	/bin/echo "[*] Exiting..."
-fi
